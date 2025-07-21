68c5b176e8e1c022ef25eb3479accb8d36ea3780
rohithktiwari <rohithktiwari@gmail.com>
2025-07-22 00:17:54 +0530

    1st project pushed

---
+++ Project1_Welcomee_Message_Generator/main.py
@@ -0,0 +1,15 @@
+#1 project 
+#1 Day1
+#WELCOME MESSAGE GENERATOR
+
+#STEP 1 ASK FOR USERS DETAILS
+name = input("Whats Your Name :- ")
+Hobby = input("Whats's Your Faviourate Hobby :- ")
+
+
+
+print("\n----welcome message ---")
+print(f"Hello {name.upper()}! 👋")
+print(f"Welocme To The World Of Python Programming")
+print(f"It's Great To Know That You Love {Hobby}")
+print(f"Get Ready To Build Something Amazing Today !")
