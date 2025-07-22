ed6ce5782b43de99072cebe1329b159b3f7d17dc
rohithktiwari <rohithktiwari@gmail.com>
2025-07-22 11:29:02 +0530

    project_2 code published

---
+++ Project2_Personalized_Greeting_Program/main.py
@@ -0,0 +1,16 @@
+#Personalized_Greeting_program
+#project-2 & Day-2
+
+#Step 1 Ask for uder details
+
+name = input("What is Your Name :- ")
+age = int(input("How Old Are You :- "))
+color = input("What's Your Faviourate Color :- ")
+
+
+#Generate a Personalized Greeting message
+print("\n----Personalized Greeting Message ------------")
+print(f"Hello{name}! 👋")
+print(f"Your are {age} Years old and {color} is a beautiful color ")
+print("You are now ready to start your python learning journey 🥰")
+
